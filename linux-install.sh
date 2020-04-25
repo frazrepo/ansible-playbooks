ebe10bae00a275c5c5b9f7ec1ff35599052d259c
frazvisuals@gmail.com <frazvisuals@gmail.com>
2020-04-25 15:44:48 +0200

    Update installer

--- linux-install.sh
+++ linux-install.sh
@@ -5,6 +5,3 @@ ansible-galaxy install -r requirements.yml
 
 # Run playbook
 ansible-playbook linux-tools.yml -i linux.hosts
-
-# Run playbook for roles
-# ansible-playbook linux-roles.yml -i linux.hosts
---
+++ windows-install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run playbook
+ansible-playbook windows-tools.yml -i windows.hosts
